fcf247626060c479c262ff05922fcf13a745797e
ohnoaaa <ohno@g-hill.jp>
2022-11-29 17:58:18 +0900

    ひととおり間量

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,50 @@
 // inview.js
 // https://github.com/protonet/jquery.inview /で対応
-$('.scroll').on('inview', function () { // .sxrollが表示されたら
-    $(this).addClass('active'); //処理を記述
-});
+// $('.scroll').on('inview', function () { // .sxrollが表示されたら
+//     $(this).addClass('active'); //処理を記述
+// });
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+//ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.blurTrigger').each(function () { //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('blur');
+      // 画面内に入ったらfadeInというクラス名を追記
+    } 
+  });
+  $('.blurslowTrigger').each(function () { //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('blurslow');
+      // 画面内に入ったらfadeInというクラス名を追記
+    } 
+  });
+  //関数でまとめることでこの後に違う動きを追加することが出来ます
+  $('.fadeDownTrigger').each(function () { //fadeInDownTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass('fadeDown');
+      // 画面内に入ったらfadeDownというクラス名を追記
+    } 
+  });
+
+
+}//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
